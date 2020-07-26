2fc8402c3f446d689ad3851dcd53c21811ac4df6
Nick Siakas <nikos.siakas@gmail.com>
2020-07-26 19:05:02 +0300

    set memory maps

--- src/libretro/LibretroEnvironment.cpp
+++ src/libretro/LibretroEnvironment.cpp
@@ -454,10 +454,8 @@ bool CLibretroEnvironment::EnvironmentCallback(unsigned int cmd, void *data)
   {
     const retro_memory_map* typedData = reinterpret_cast<const retro_memory_map*>(data);
     if (typedData)
-    {
-      // Not implemented
-      return false;
-    }
+      m_mmap.Initialize(*typedData);
+
     break;
   }
   case RETRO_ENVIRONMENT_SET_GEOMETRY:
@@ -613,4 +611,9 @@ bool CLibretroEnvironment::EnvironmentCallback(unsigned int cmd, void *data)
   }
 
   return true;
+}
+
+const CMemoryMap& CLibretroEnvironment::GetMemoryMap()
+{
+  return m_mmap;
 }
